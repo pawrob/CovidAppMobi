6496dd7831fc18ca564814d25fb70d65a1fc21e9
r00bertos1 <69626763+r00bertos1@users.noreply.github.com>
2021-01-26 21:51:02 +0100

    ADDED [Advice page]

--- app/src/main/java/com/example/covidbook/ui/emergency/EmergencyActivity.java
+++ app/src/main/java/com/example/covidbook/ui/emergency/EmergencyActivity.java
@@ -2,19 +2,12 @@ package com.example.covidbook.ui.emergency;
 
 import android.os.Bundle;
 
-import com.example.covidbook.R;
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
-import com.google.android.material.tabs.TabLayout;
-
-import androidx.viewpager.widget.ViewPager;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
 
-import android.view.Menu;
-import android.view.MenuItem;
-import android.view.View;
-
+import com.example.covidbook.R;
 import com.example.covidbook.ui.emergency.ui.main.SectionsPagerAdapter;
+import com.google.android.material.tabs.TabLayout;
 
 public class EmergencyActivity extends AppCompatActivity {
 
@@ -27,6 +20,5 @@ public class EmergencyActivity extends AppCompatActivity {
         viewPager.setAdapter(sectionsPagerAdapter);
         TabLayout tabs = findViewById(R.id.tabs);
         tabs.setupWithViewPager(viewPager);
-
     }
 }
--- app/src/main/java/com/example/covidbook/ui/emergency/SecondFragment.java
+++ app/src/main/java/com/example/covidbook/ui/emergency/SecondFragment.java
@@ -1,19 +1,23 @@
 package com.example.covidbook.ui.emergency;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
-import androidx.navigation.fragment.NavHostFragment;
 
 import com.example.covidbook.R;
+import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.views.YouTubePlayerView;
 
 public class SecondFragment extends Fragment {
 
     private static final String ARG_SECTION_NUMBER = "section_number";
+    private Button backButton;
 
     public static SecondFragment newInstance(int index) {
         SecondFragment fragment = new SecondFragment();
@@ -27,7 +31,26 @@ public class SecondFragment extends Fragment {
     public View onCreateView(
             LayoutInflater inflater, ViewGroup container,
             Bundle savedInstanceState) {
+        View root = inflater.inflate(R.layout.fragment_second, container, false);
+        YouTubePlayerView youTubePlayerView = root.findViewById(R.id.youtube_player_view);
+        getLifecycle().addObserver(youTubePlayerView);
+
         return inflater.inflate(R.layout.fragment_second, container, false);
+    }
+
+    public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        backButton = (Button) view.findViewById(R.id.adv_end_btn);
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @SuppressLint("SetTextI18n")
+            @Override
+            public void onClick(View view) {
+
+                Intent startIntent = new Intent(getActivity(), com.example.covidbook.MainActivity.class);
+                startActivity(startIntent);
+            }
+        });
     }
 
 }
