88d574e121d72d3b4a3a3814bedbccd8b6d784d8
r00bertos1 <69626763+r00bertos1@users.noreply.github.com>
2021-01-26 01:05:15 +0100

    ADDED [Icon and SplashActivity]

--- app/src/main/java/com/example/covidbook/MainActivity.java
+++ app/src/main/java/com/example/covidbook/MainActivity.java
@@ -1,14 +1,6 @@
 package com.example.covidbook;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-
-import android.widget.Button;
-
-
-import com.example.covidbook.ui.home.HomeFragment;
-import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.NavController;
@@ -16,10 +8,13 @@ import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         BottomNavigationView navView = findViewById(R.id.nav_view);
