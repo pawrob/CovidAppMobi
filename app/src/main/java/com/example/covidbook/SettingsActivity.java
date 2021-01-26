21a596c8bd77c905fa2ea3e8f57dc09e72c895a8
r00bertos1 <69626763+r00bertos1@users.noreply.github.com>
2021-01-26 18:18:25 +0100

    ADDED [Toggleable dark mode]

--- app/src/main/java/com/example/covidbook/SettingsActivity.java
+++ app/src/main/java/com/example/covidbook/SettingsActivity.java
@@ -1,14 +1,13 @@
 package com.example.covidbook;
 
-import android.content.Intent;
+import android.content.res.Configuration;
 import android.os.Bundle;
 
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
 import androidx.preference.PreferenceFragmentCompat;
-
-import com.example.covidbook.info.age.AgeActivity;
-import com.example.covidbook.info.name.NameActivity;
+import androidx.preference.SwitchPreferenceCompat;
 
 public class SettingsActivity extends AppCompatActivity {
 
@@ -32,6 +31,25 @@ public class SettingsActivity extends AppCompatActivity {
         @Override
         public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
             setPreferencesFromResource(R.xml.root_preferences, rootKey);
+            SwitchPreferenceCompat nightMode = findPreference("dark_mode_toggle");
+            if (nightMode != null) {
+                nightMode.setOnPreferenceChangeListener((preference, newValue) -> {
+                    switch (getResources().getConfiguration().uiMode & Configuration.UI_MODE_NIGHT_MASK) {
+                        case Configuration.UI_MODE_NIGHT_YES:
+                            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+                            break;
+                        case Configuration.UI_MODE_NIGHT_NO:
+                            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+                            break;
+                    }
+                    return true;
+                });
+            }
+//            PreferenceManager.getDefaultSharedPreferences.getBoolean(
+//            resources.getString(R.string.my_bool_setting_key),
+//            resources.getBoolean(R.bool.my_bool_setting_default))
         }
+
+
     }
 }
--- app/src/main/java/com/example/covidbook/info/age/AgeFragment.java
+++
@@ -1,4 +0,0 @@
-package com.example.covidbook.info.age;
-
-public class AgeFragment {
-}
